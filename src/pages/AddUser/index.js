a84ae4740d024041a6ad7db8c00bfb2c321479c7
“gsnorberto” <sn_gabriel@outlook.com>
2023-01-25 20:25:51 -0300

    style: Adiciona efeito de carregamento no botão de cadastro

--- src/pages/AddUser/index.js
+++ src/pages/AddUser/index.js
@@ -1,6 +1,7 @@
 import { SignUpArea, Logo, Form, Input, Button, Link } from "./styles"
 import { useState } from "react"
 import { NavLink, useNavigate } from "react-router-dom"
+import { ThreeDots } from "react-loader-spinner";
 import axios from "axios"
 
 export default () => {
@@ -9,24 +10,27 @@ export default () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [loading, setLoading] = useState(false)
 
     const addUser = (e) => {
         e.preventDefault()
         console.log(process.env.REACT_APP_API_URL);
+        setLoading(true)
 
-        if(!name || !email || !password || !confirmPassword){
+        if (!name || !email || !password || !confirmPassword) {
             return alert("Preencha todos os campos! ")
         }
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             return alert("As senhas não conferem! ")
         }
 
-        let data = {name, email, password, confirmPassword}
-        axios.post(process.env.REACT_APP_API_URL+"/auth/sign-up", data)
+        let data = { name, email, password, confirmPassword }
+        axios.post(process.env.REACT_APP_API_URL + "/auth/sign-up", data)
             .then((res) => {
+                setLoading(false)
                 navigate("/")
             }).catch((error) => {
-                console.log(error);
+                setLoading(false)
                 alert("Error: " + error.response.data)
             })
     }
@@ -65,7 +69,19 @@ export default () => {
                     value={confirmPassword}
                     onChange={e => setConfirmPassword(e.target.value)}
                 />
-                <Button data-test="sign-up-submit" type="submit">Cadastrar</Button>
+                <Button data-test="sign-up-submit" type="submit">
+                    <ThreeDots
+                        height="30"
+                        width="40"
+                        radius="9"
+                        color="#FFFFFF"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{}}
+                        wrapperClassName=""
+                        visible={loading}
+                    />
+                    {loading ? '' : 'Cadastrar'}
+                </Button>
             </Form>
             <Link> <NavLink to="/">Já tem uma conta? Entre agora!</NavLink></Link>
         </SignUpArea>
