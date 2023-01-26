054f83a533ca9479bd471ecaee7e6270cad966b0
FabioGarroteDev <fgarrote12@gmail.com>
2023-01-26 14:56:37 -0300

    style: estilização inicial feita da página de detalhes do produto

--- src/pages/ProductDetail/styles.js
+++ src/pages/ProductDetail/styles.js
@@ -1,1 +1,82 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ProductContainer = styled.div`
+  width: 50%;
+  display: flex;
+  margin-top: 250px;
+  gap: 20px;
+  img {
+    width: 450px;
+    height: 500px;
+  }
+`
+
+export const Informations = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: Arial, Helvetica, sans-serif;
+  gap: 20px;
+  h1 {
+    font-size: 40px;
+    font-weight: 700;
+    color: #292929;
+  }
+  h2 {
+    font-size: 20px;
+    font-weight: 700;
+  }
+  h3 {
+    font-size: 15px;
+    font-weight: 700;
+    color: #767676;
+  }
+  p {
+    font-size: 15px;
+    color: #767676;
+    line-height: 20px;
+  }
+  select {
+    width: 300px;
+    height: 50px;
+    border-radius: 5px;
+    font-size: 15px;
+    background-color: #FFF;
+    outline: none;
+    padding-left: 10px;
+    border: 1px solid #767676;
+    cursor: pointer;
+  }
+  div {
+    display: flex;
+    width: 100px;
+    gap: 5px;
+    align-items: center;
+    p {
+      border: 1px solid #767676;
+      width: 40px;
+      text-align: center;
+      border-radius: 5px;
+      font-size: 15px;
+    }
+    svg {
+      color: #000;
+      cursor: pointer;
+    }
+  }
+  button {
+    width: 100%;
+    height: 50px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    color: #FFF;
+    background-color: #292929;
+    font-size: 17px;
+  }
+`
