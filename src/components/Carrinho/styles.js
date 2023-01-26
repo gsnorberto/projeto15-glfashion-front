6b9a156d1bafaf723315444902d61c75922e693a
Laura <laurasouzacastro1@gmail.com>
2023-01-25 21:11:37 -0300

    feat: funcionalidades do carrinho

---
+++ src/components/Carrinho/styles.js
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+export const Fundo = styled.div`
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    display: ${props => props.carrinho ? "flex" : "none"};
+`
+
+export const CarrinhoContainer = styled.div`
+    width: 40%;
+    height: 100vh;
+    background-color: #FFFFFF;
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 2;
+    display: ${props => props.carrinho ? "flex" : "none"};
+    flex-direction: column;
+    padding: 0px 20px;
+    p{
+        align-self: center;
+        color: #555555;
+    }
+`
+
+export const Topo = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    padding: 20px 0px;
+    font-size: 20px;
+    p{
+        padding: 0px 10px;
+    }
+    span{
+        color: red;
+        padding: 0px 5px;
+    }
+`
+export const Rodape = styled.div`
+    width: 100%;
+    height: 20vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background-color: #FFFFFF;
+    div{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    button{
+        border: none;
+        width: 300px;
+        height: 40px;
+        margin-top: 20px;
+        border-radius: 5px;
+        color: #FFFFFF;
+        background-color: #000000;
+        font-size: 17px;
+    }
+`
+export const Produtos = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 70%;
+    overflow-y: scroll;
+`
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import CardProduto from "../../components/CardProduto/index.js"
 import HeaderMenu from "../../components/HeaderMenu/index.js"
+import Carrinho from "../../components/Carrinho/index.js"
 import { CardsContainer, Container } from "./styles.js"
 
 export default () => {
@@ -11,10 +14,22 @@ export default () => {
             valor_com_desconto: "97,50"
         }
     ]
-
+    const [carrinho, setCarrinho] = useState(false)
+    const [compras, setCompras] = useState([])
+    useEffect(() => {
+        const requisicao = axios.get(`${process.env.REACT_APP_API_URL}/purchases`);
+        requisicao.then((res) => setCompras(res.data));
+        requisicao.catch((res) => alert(res.response.data));  
+    }, [ setCompras ]);
+    function atualizaCompras(){
+        const requisicao = axios.get(`${process.env.REACT_APP_API_URL}/purchases`);
+        requisicao.then((res) => setCompras(res.data));
+        requisicao.catch((res) => alert(res.response.data));  
+    }
     return (
         <Container>
-            <HeaderMenu />
+            <HeaderMenu compras={compras} carrinho={carrinho} setCarrinho={setCarrinho}/>
+            <Carrinho compras={compras} carrinho={carrinho} setCarrinho={setCarrinho} atualizaCompras={atualizaCompras}/>
             <CardsContainer>
                 {PRODUTOS.map(item => <CardProduto nome={item.nome} imagem={item.url_imagem} valor={item.valor} desconto={item.valor_com_desconto}/>)}
             </CardsContainer>
