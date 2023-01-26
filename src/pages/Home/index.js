33cc0cb28d2fa6a94ca1a01c74fc9641862d21b5
Laura <laurasouzacastro1@gmail.com>
2023-01-26 16:00:10 -0300

    fix: passando idUsuario como parametro na requisição get

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,30 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
 import axios from 'axios'
 import CardProduto from "../../components/CardProduto/index.js"
 import HeaderMenu from "../../components/HeaderMenu/index.js"
 import Carrinho from "../../components/Carrinho/index.js"
 import { CardsContainer, Container } from "./styles.js"
+import { Context } from "../../context/AuthContext"
 
 export default () => {
 	const [products, setProducts] = useState([]);
   const [carrinho, setCarrinho] = useState(false)
   const [compras, setCompras] = useState([])
+  let { userLS } = useContext(Context)
 
 	useEffect(() => {
 		const URL = `${process.env.REACT_APP_API_URL}/all-products`;
 		const promise = axios.get(URL);
 		promise.then(res => setProducts(res.data));
     promise.catch(err => console.log(err.response.data))
-    
-    const requisicao = axios.get(`${process.env.REACT_APP_API_URL}/purchases`);
+
+    const requisicao = axios.get(`${process.env.REACT_APP_API_URL}/purchases/${userLS.id}`);
     requisicao.then((res) => setCompras(res.data));
     requisicao.catch((res) => alert(res.response.data));  
 	}, [setCompras]);
 
 	function atualizaCompras(){
-    const requisicao = axios.get(`${process.env.REACT_APP_API_URL}/purchases`);
+    const requisicao = axios.get(`${process.env.REACT_APP_API_URL}/purchases/${userLS.id}`);
     requisicao.then((res) => setCompras(res.data));
     requisicao.catch((res) => alert(res.response.data));
   }
